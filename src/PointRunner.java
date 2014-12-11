20185b10367d990de6be02e4d60a3569cb39a684
mpearce25 <pearcematthew26@gmail.com>
2014-12-11 06:39:07 -0800

    moved point class

--- src/PointRunner.java
+++ src/PointRunner.java
@@ -5,9 +5,6 @@
  * @author Mr. Chetlen
  * @version Decemeber 2014
  */
-// Java1002.java
-// The <Point> class is improved with a second "overloaded" constructor.
-
 
 public class PointRunner
 {
@@ -16,35 +13,11 @@ public class PointRunner
       Point point1 = new Point();
       System.out.println("Point1 at (" + point1.getX() + "," + point1.getY() + ")");
 
-      //Point point2 = new Point(500,300);
-     // System.out.println("Point2 at (" + point2.getX() + "," + point2.getY() + ")");
+      Point point2 = new Point(500,300);
+      System.out.println("Point2 at (" + point2.getX() + "," + point2.getY() + ")");
     }
 }
 
 
-class Point
-{
-   private int x;
-   private int y;
 
-   public Point()
-   {
-      x = 0;
-      y = 0;
-   }
 
-   //Add the overloaded Point constructor method here.
-
-   public int getX()
-   {
-      return x;
-   }
-
-   public int getY()
-   {
-      return y;
-   }
-
-   
-   // Add setX and setY methods here.
-}
